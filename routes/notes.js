a2fa16f03dbe21e370ab19a3c3c65451669a5e63
Katsuyuki <katsuyuki.russell@gmail.com>
2023-08-01 14:23:16 -0500

    added get and post routes

--- routes/notes.js
+++ routes/notes.js
@@ -1,2 +1,29 @@
 const notes = require('express').Router();
 const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
+
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new note
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+
+    if (title && text) {
+        var id = Date.now();
+        const newNote = {
+            title,
+            text,
+            id
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully 🚀`);
+    } else {
+        res.error('Error in adding Note');
+    }
+});
+
+
+module.exports = notes;
